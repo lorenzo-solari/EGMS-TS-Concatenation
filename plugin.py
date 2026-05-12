0bcf80866dc012ed3046dd104558d940aaf80b72
lorenzo-solari <134623104+lorenzo-solari@users.noreply.github.com>
2026-05-12 13:17:47 +0200

    Refactor run method to create new dialog instance
    Updated the run method to instantiate the dialog each time for fresh layer combos.

--- plugin.py
+++ plugin.py
@@ -3,17 +3,21 @@ from qgis.PyQt.QtGui import QIcon
 import os
 from .dialog import EGMSDialog
 
+
 class EGMS_TS_Concatenation:
 
     def __init__(self, iface):
-        self.iface = iface
+        self.iface      = iface
         self.plugin_dir = os.path.dirname(__file__)
-        self.action = None
-        self.dialog = None
+        self.action     = None
 
     def initGui(self):
-        icon_path = os.path.join(self.plugin_dir, "icon.png")
-        self.action = QAction(QIcon(icon_path), "EGMS Time Series Concatenation", self.iface.mainWindow())
+        icon_path   = os.path.join(self.plugin_dir, "icon.png")
+        self.action = QAction(
+            QIcon(icon_path),
+            "EGMS Time Series Concatenation",
+            self.iface.mainWindow()
+        )
         self.action.triggered.connect(self.run)
         self.iface.addToolBarIcon(self.action)
         self.iface.addPluginToMenu("&EGMS Tools", self.action)
@@ -23,6 +27,6 @@ class EGMS_TS_Concatenation:
         self.iface.removePluginMenu("&EGMS Tools", self.action)
 
     def run(self):
-        if not self.dialog:
-            self.dialog = EGMSDialog(self.iface)
-        self.dialog.show()
+        # Instantiate fresh each time so the layer combos are always up to date
+        dlg = EGMSDialog(self.iface)
+        dlg.exec()
